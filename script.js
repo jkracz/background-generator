97df9d2be088659a4050e6f0419ffaa67b1ba0b8
jkracz <joseph.kracz@gmail.com>
2022-09-02 21:42:40 -0400

    updating files, making sure node modules aren't included

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+ // import syntax comes with es6, and we can use it with webpack
+ //import { without } from "lodash";
+
+ // before es6, we needed to import packages using browserify
+ var _ = require('lodash');
+
+ var arr = [1,2,3,4,5,6,7,8];
+ console.log(_.without(arr, 3));
+ // browserify isn't used as much anymore, but you can use it to create a bundle in cases where you can't use webpack
+
  var css = document.querySelector("h3");
  var color1 = document.querySelector(".color1");
  var color2 = document.querySelector(".color2");
